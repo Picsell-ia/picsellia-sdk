94bc2508cc46bf71bbacdbca354033e2ff7a00a6
Thibaut Lucas <thibautlucas13@gmail.com>
2020-05-22 10:03:44 +0200

    v1

--- test/test_picsellia.py
+++ test/test_picsellia.py
@@ -3,13 +3,12 @@ import unittest
 
 
 class TestClient(unittest.TestCase):
-    def test_init_local(self):
+    def test_init_local_pic(self):
         client = Client(token="8054234f-408e-4aee-ad4f-5346ff572d46",
                         host="https://backstage.picsellia.com/sdk/",
                         png_dir='/home/batman/Documents/PicsellAL/PennFudanPed/PNGImages')
         self.assertEqual(client.token, "8054234f-408e-4aee-ad4f-5346ff572d46")
-        self.assertTrue(isinstance(client.project_id, str))
-        self.assertTrue(isinstance(client.png_dir, str))
+
 
     def test_init_cloud(self):
         client = Client(token="8054234f-408e-4aee-ad4f-5346ff572d46",
