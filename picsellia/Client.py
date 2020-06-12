d4e209dd2f81c53e1f0631cb88809318169a413e
BouleJaune <amthierry81@live.fr>
2020-06-12 10:37:38 +0200

    fix

--- picsellia/Client.py
+++ picsellia/Client.py
@@ -1536,16 +1536,16 @@ class Client:
 
                                 poly = np.array(poly, dtype=np.float32)
 
-                                    x, y, w, h = cv2.boundingRect(poly)
-                                    x1_norm = np.clip(x/width, 0, 1) 
-                                    x2_norm = np.clip((x+w)/width, 0, 1) 
-                                    y1_norm = np.clip(y/height, 0, 1) 
-                                    y2_norm = np.clip((y+h)/height, 0, 1) 
+                                x, y, w, h = cv2.boundingRect(poly)
+                                x1_norm = np.clip(x/width, 0, 1) 
+                                x2_norm = np.clip((x+w)/width, 0, 1) 
+                                y1_norm = np.clip(y/height, 0, 1) 
+                                y2_norm = np.clip((y+h)/height, 0, 1) 
 
-                                    xmins.append(x1_norm)
-                                    xmaxs.append(x2_norm)
-                                    ymins.append(y1_norm)
-                                    ymaxs.append(y2_norm)
+                                xmins.append(x1_norm)
+                                xmaxs.append(x2_norm)
+                                ymins.append(y1_norm)
+                                ymaxs.append(y2_norm)
                                 classes_text.append(a["label"].encode("utf8"))
                                 label_id = label_map[a["label"]]
                                 classes.append(label_id)
@@ -1595,7 +1595,7 @@ class Client:
                                     # xmax = xmin + a["rectangle"]["height"]
                                     # ymin = a["rectangle"]["left"]
                                     # ymax = ymin + a["rectangle"]["width"]
-                                    
+
                                     ymins.append(np.clip(ymin/height, 0, 1))
                                     ymaxs.append(np.clip(ymax/height, 0, 1))
                                     xmins.append(np.clip(xmin/width, 0, 1))
