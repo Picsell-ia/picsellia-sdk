6e24384724fcfa71f903e7dc75990bd323fbb0ea
BouleJaune <amthierry81@live.fr>
2020-07-22 11:06:22 +0200

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='picsellia',
     packages=['picsellia'],
-    version='4.1',
+    version='4.2',
 
     license='MIT',
     description='Python SDK to make your code Picsell.ia compatible !',
